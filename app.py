4a85340d02ed98df766d0fb123000d5e93cad94e
Sara Stiklickas <sara.stiklickas@ge.com>
2017-09-28 16:26:16 -0400

    WIP setting up authentication

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+CLIENT_ID = "1d6559453b7045208e227f50b7276401"
+CLIENT_SECRET = "eefd28242abf4b77a94f43495d7be0e7"
+
 @app.route("/")
 def home():
     return render_template("index.html")
